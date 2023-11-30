818f2eba557aa475f048f18cdf30bb6c61231492
stiles667 <i.elyamani@ecole-ipssi.net>
2023-11-30 15:58:35 +0100

    Update modif.php

--- src/modif.php
+++ src/modif.php
@@ -6,15 +6,12 @@ require './config.php';
 $your_db_instance = new Database();
 $recipe = new Recette($your_db_instance);
 
-// Vérifie si le paramètre 'id' est défini dans l'URL
-$id = isset($_GET['id']) ? $_GET['id'] : null;
-
 // Vérifie si le formulaire a été soumis
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Assurez-vous que les champs nécessaires sont présents dans le formulaire
     if (isset($_POST['nom'], $_POST['image'], $_POST['difficulte'], $_POST['temps_preparation'], $_POST['ustensiles'], $_POST['id_categorie'])) {
         // Remplissez les données de la recette
-        $recipe->id = $id;
+        $recipe->id = $_POST['id'];
         $recipe->nom = $_POST['nom'];
         $recipe->image = $_POST['image'];
         $recipe->difficulte = $_POST['difficulte'];
@@ -34,55 +31,37 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo 'Veuillez remplir tous les champs du formulaire.';
     }
 } else {
-    // Si l'ID n'est pas fourni, redirigez l'utilisateur ou affichez un message d'erreur
-    if (!$id) {
-        echo 'L\'ID de la recette n\'est pas fourni.';
-        exit;
-    }
+    // Affichez toutes les recettes disponibles
+    $recettes = $recipe->ajouterRecette();
 
-    // Obtenez les données de la recette
-    $recipeData = $recipe->getRecipeById($id);
+    echo '<h1>All Recipes:</h1>';
 
-    // Vérifiez si la recette existe
-    if (!$recipeData) {
-        echo 'La recette avec cet ID n\'existe pas.';
-        exit;
+    echo '<ul>';
+    while ($recette = $recettes->fetch(PDO::FETCH_ASSOC)) {
+        echo '<li><a href="modif.php?id=' . $recette['id'] . '">' . $recette['nom'] . '</a></li>';
     }
+    echo '</ul>';
+
+    // Si l'ID est défini dans l'URL, affichez le formulaire de modification
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $recipeData = $recipe->getRecipeById($id);
+
+        if ($recipeData) {
+            echo '<h2>Edit Recipe</h2>';
+            echo '<form action="modif.php" method="post">';
+            echo '<input type="hidden" name="id" value="' . $recipeData['id'] . '">';
+            echo 'Recipe Name: <input type="text" name="nom" value="' . $recipeData['nom'] . '"><br>';
+            echo 'Image URL: <input type="text" name="image" value="' . $recipeData['image'] . '"><br>';
+            echo 'Difficulty: <input type="text" name="difficulte" value="' . $recipeData['difficulte'] . '"><br>';
+            echo 'Preparation Time (minutes): <input type="text" name="temps_preparation" value="' . $recipeData['temps_preparation'] . '"><br>';
+            echo 'Utensils: <input type="text" name="ustensiles" value="' . $recipeData['ustensiles'] . '"><br>';
+            echo 'Category: <input type="text" name="id_categorie" value="' . $recipeData['id_categorie'] . '"><br>';
+            echo '<input type="submit" value="Submit">';
+            echo '</form>';
+        } else {
+            echo 'La recette avec cet ID n\'existe pas.';
+        }
+    }
+}
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Recipe</title>
-</head>
-<body>
-<h1>Choisissez une recette à modifier :</h1>
-
-<ul>
-    <?php foreach ($recettes as $recette) : ?>
-        <li>
-            <a href="modifier_recette.php?id=<?php echo $recette['id']; ?>">
-                <?php echo $recette['nom']; ?>
-            </a>
-        </li>
-    <?php endforeach; ?>
-</ul>
-
-<h2>Edit Recipe</h2>
-
-<form action="modif.php?id=<?php echo $id; ?>" method="post">
-    Recipe Name: <input type="text" name="nom" value="<?php echo $recipeData['nom']; ?>"><br>
-    Image URL: <input type="text" name="image" value="<?php echo $recipeData['image']; ?>"><br>
-    Difficulty: <input type="text" name="difficulte" value="<?php echo $recipeData['difficulte']; ?>"><br>
-    Preparation Time (minutes): <input type="text" name="temps_preparation" value="<?php echo $recipeData['temps_preparation']; ?>"><br>
-    Utensils: <input type="text" name="ustensiles" value="<?php echo $recipeData['ustensiles']; ?>"><br>
-    Category: <input type="text" name="id_categorie" value="<?php echo $recipeData['id_categorie']; ?>"><br>
-    <input type="submit" value="Submit">
-</form>
-
-</body>
-</html>
-
-<?php } ?>
