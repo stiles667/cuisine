67f3148cde5a8ef0155b5bba3f8eebd998deb0fe
lenzees <119585553+lenzees@users.noreply.github.com>
2023-12-03 22:24:47 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -27,7 +27,7 @@
     // Connexion à la base de données
     $servername = "localhost";
     $username = "root";
-    $password = "Khaled";
+    $password = "";//entrez votre mot de passe
     $dbname = "cuisine";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
